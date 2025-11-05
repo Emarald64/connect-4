20ff27cdae81166fceae6a644363d9af445270a8
Xanderath <94411022+Emarald64@users.noreply.github.com>
2025-11-05 09:05:41 -0500

    added scoring

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Connect 4
+
+![hackatime](https://hackatime-badge.hackclub.com/U0785D5VDEK/connect-4)
+
+Platforming game where you switch gravity instead of jump.
+
+Made with the Godot Engine
+
+## To play:
+
+Run the web version or download the desktop builds from the game's page on [itch.io](https://xanderath.itch.io/)
+
+or
+
+1. Download Godot from https://godotengine.org/
+
+2. Download the repo using
+``git clone https://github.com/Emarald64/connect4.git``
+
+4. Import the folder you cloned the repo into Godot 
+
+<img width="242" height="81" alt="Screenshot_20251008_090434" src="https://github.com/user-attachments/assets/1e9504e7-3593-41b7-af27-6276a2f9500b" />
+
+Then you can play or edit the project
--- scenes/main.gd
+++ scenes/main.gd
@@ -18,9 +18,9 @@ func _ready() -> void:
 			slots[y].append(slot)
 			%Board.add_child(slot)
 
-func moveWithChangingColor(col:int)->bool:
-	currentlyRed=not currentlyRed
-	return move(col,not currentlyRed)
+func moveWithChangingColor(col:int)->void:
+	if move(col,currentlyRed):
+		currentlyRed=not currentlyRed
 
 func move(col:int,red:bool)->bool:
 	if slots[0][col].filled:
@@ -34,8 +34,33 @@ func move(col:int,red:bool)->bool:
 	slots[row][col].red=red
 	slots[row][col].get_node('Piece').show()
 	slots[row][col].get_node('Piece').modulate=Color(1,0,0) if red else Color(0.0, 0.6, 1.0, 1.0)
-	score()
+	var score:=scoreBoard()
+	if score!=&"none":
+		print(score+" Won!")
 	return true
 	
-func score()->void:
-	pass
+func scoreBoard()->StringName:
+	#Verticals
+	for y in range(3):
+		for x in range(7):
+			if slots[y][x].red==slots[y+1][x].red and slots[y][x].red==slots[y+2][x].red and slots[y][x].red==slots[y+3][x].red and slots[y][x].filled and slots[y+1][x].filled and slots[y+2][x].filled and slots[y+3][x].filled:
+				return &"red" if slots[y][x].red else &"blue"
+				
+	# Horazontals
+	for y in range(6):
+		for x in range(4):
+			if slots[y][x].red==slots[y][x+1].red and slots[y][x].red==slots[y][x+2].red and slots[y][x].red==slots[y][x+3].red and slots[y][x].filled and slots[y][x+1].filled and slots[y][x+2].filled and slots[y][x+3].filled:
+				return &"red" if slots[y][x].red else &"blue"
+				
+	# down-right Diagonals
+	for y in range(3):
+		for x in range(4):
+			if slots[y][x].red==slots[y+1][x+1].red and slots[y][x].red==slots[y+2][x+2].red and slots[y][x].red==slots[y+3][x+3].red and slots[y][x].filled and slots[y+1][x+1].filled and slots[y+2][x+2].filled and slots[y+3][x+3].filled:
+				return &"red" if slots[y][x].red else &"blue"
+				
+	# up-right diagonals
+	for y in range(3):
+		for x in range(4):
+			if slots[y][x+3].red==slots[y+1][x+2].red and slots[y][x+3].red==slots[y+2][x+1].red and slots[y][x+3].red==slots[y+3][x].red and slots[y+3][x].filled and slots[y+2][x+1].filled and slots[y+1][x+2].filled and slots[y+3][x].filled:
+				return &"red" if slots[y][x].red else &"blue"
+	return &"none"
